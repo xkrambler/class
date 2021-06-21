70f94bb1c68f39eb255038cba56c552f5fc7836a
Dr.Chester <github@xkr.es>
2021-06-21 13:57:11 +0200

    jsmail: mejoras, jsmails

--- jsmail.php
+++ jsmail.php
@@ -1,32 +1,52 @@
 <?php
 
-	// cifrar email en javascript y devolver como document.write
-	function jsmail($m,$a=false,$aa="") {
-		$words=Array();
-		for ($i=0;$i<strlen($m);$i++) {
-			$c=substr($m,$i,1);
-			$l=$words[$c];
-			$words[$c][($words[$c]?count($words[$c]):0)]=$i;
-		}
-		$t="";
-		foreach ($words as $l=>$p) {
-			$t.=($t?",":"")."'".$l."':[";
-			for ($i=0;$i<count($p);$i++)
-				$t.=($i?",":"").$p[$i];
-			$t.="]";
-		}
-		$b="<script>
-			var m={".$t."};
-			var t='".str_repeat(" ",strlen($m))."';
-			for (var i in m)
-				for (var j in m[i])
-					t=t.substring(0,m[i][j])+i+t.substring(m[i][j]+1);
-			document.write(".($a?"\"<a href='mailto:\"+t+\"'".($aa?" ".$aa:"").">\"+t+\"</a>\"":"t").");
-		</script>";
-		return $b;
+// cifrar email en javascript y devolver como script devolviendo variable m
+function jsmails($m) {
+	$words=array();
+	for ($i=0; $i<strlen($m); $i++) {
+		$c=substr($m, $i, 1);
+		$l=$words[$c];
+		$words[$c][($words[$c]?count($words[$c]):0)]=$i;
 	}
+	$t="";
+	foreach ($words as $l=>$p) {
+		$t.=($t?",":"")."'".$l."':[";
+		for ($i=0; $i<count($p); $i++)
+			$t.=($i?",":"").$p[$i];
+		$t.="]";
+	}
+	return "
+		var w={".$t."};
+		var m='".str_repeat(" ", strlen($m))."';
+		for (var i in w)
+			for (var j in w[i])
+				m=m.substring(0, w[i][j])+i+m.substring(w[i][j]+1);
+	";
+}
 
-	// crea un enlace a un correo cifrado
-	function jsmaila($m,$t="",$aa="") {
-		return (jsmail($m,true,($t?"target='".$t."'":"").($aa?" ".$aa:"")));
+// cifrar email en javascript y devolver como document.write
+function jsmail($m,$a=false,$aa="") {
+	$words=Array();
+	for ($i=0;$i<strlen($m);$i++) {
+		$c=substr($m,$i,1);
+		$l=$words[$c];
+		$words[$c][($words[$c]?count($words[$c]):0)]=$i;
 	}
+	$t="";
+	foreach ($words as $l=>$p) {
+		$t.=($t?",":"")."'".$l."':[";
+		for ($i=0;$i<count($p);$i++)
+			$t.=($i?",":"").$p[$i];
+		$t.="]";
+	}
+	$b="<script>
+		".jsmails($m)."
+		document.write(".($a?"\"<a href='mailto:\"+m+\"'".($aa?" ".$aa:"").">\"+m+\"</a>\"":"m").");
+	</script>";
+	return $b;
+}
+
+// cifrar email en javascript y devolver como enlace document.write
+function jsmaila($m,$t="",$aa="") {
+	return (jsmail($m,true,($t?"target='".$t."'":"").($aa?" ".$aa:"")));
+}
