f759dedd580b35cbb99db6e947811437c2345e9c
xkrambler <github@xkr.es>
2022-01-08 16:59:21 +0100

    line endings, balance, customer, source, request

--- xstripe.php
+++ xstripe.php
@@ -8,7 +8,7 @@
 		4242 4242 4242 4242 - No authentication (default U.S. card)
 		4000 0027 6000 3184 - Authentication required
 		4000 0000 0000 9995 - Always fails with a decline code of insufficient_funds.
-	
+
 	Example usage:
 
 		// instance Stripe Payment
@@ -39,9 +39,11 @@
 class xStripe {
 
 	protected $defaults=[
+		"version"=>"0.1",
 		"ajax"=>"xstripe.pay",
 		"currency"=>"eur",
 		"metadata"=>[],
+		"base"=>"https://api.stripe.com/",
 	];
 	protected $error="";
 	protected $o;
@@ -56,6 +58,77 @@ class xStripe {
 	function defaults() { return $this->defaults; }
 	function get() { return $this->o; }
 	function set(array $a) { foreach ($a as $k=>$v) $this->o[$k]=$v; }
+
+	// get account balance
+	function balance() {
+		return $this->request("v1/balance");
+	}
+
+	// create customer
+	function customer(array $o) {
+		return $this->request("v1/customers");
+	}
+
+	// create payment source
+	function source(array $o) {
+		return $this->request("v1/sources", $o);
+	}
+
+	// do request
+	function request($url, $fields=null, $o=[]) {
+
+		// CURL requisite
+		if (!function_exists("curl_init")) return $this->error("xStripe: CURL module not installed.");
+
+		// prepare POST
+		if ($fields !== null) {
+			$o[CURLOPT_POST]=1;
+			$o[CURLOPT_POSTFIELDS]=(is_array($fields)?http_build_query($fields):$fields);
+		}
+
+		// CURL request
+		$ch=curl_init();
+		curl_setopt_array($ch, $o+[
+			//CURLOPT_VERBOSE=>1, // CAUTION: only for debuging purposes
+			CURLOPT_URL=>$this->base.$url,
+			CURLOPT_FRESH_CONNECT=>1,
+			CURLOPT_RETURNTRANSFER=>1,
+			CURLOPT_FORBID_REUSE=>1,
+			//CURLOPT_HEADER=>1,
+			//CURLINFO_HEADER_OUT=>1,
+			CURLOPT_HTTPHEADER=>[
+				"Authorization: Bearer ".$this->sk,
+				"User-Agent: xStripe ".$this->version,
+				//"Content-Type: application/json; charset=UTF-8",
+				//"Expect:",
+			],
+		]);
+		$this->response=curl_exec($ch);
+		$this->info=curl_getinfo($ch);
+		file_put_contents("data/curldebug.txt", "\n[REQUEST]\n".json_encode($this->info, JSON_PRETTY_PRINT)."\n[POST]\n".$post."\n[RESPONSE]\n".$this->response."\n\n", FILE_APPEND);
+
+		// separate headers
+		/*if ($i=strpos($r, "\r\n\r\n")) {
+			$h=explode("\r\n", substr($r, 0, $i));
+			$r=substr($r, $i+4);
+		} else {
+			$h=[$r];
+			$r="";
+		}
+		$f["headers"]=[];
+		if ($h) for ($i=1; $i<count($h); $i++) {
+			$l=$h[$i];
+			$p=strpos($l, ": ");
+			$f["headers"][strtolower(substr($l, 0, $p))]=substr($l, $p+2);
+		}*/
+
+		// close CURL connection
+		curl_close($ch);
+
+		// return response as array
+		return json_decode($this->response, true);
+
+	}
 
 	// process payment
 	function process(array $o=[]) {
@@ -101,11 +174,10 @@ class xStripe {
 		*/
 		} catch (\Stripe\Exception\ApiErrorException $e) {
 			// display a very generic error to the user, and maybe send yourself an email
-			ajax(["err"=>$e->getMessage()]);
-		// something else happened, completely unrelated to Stripe
-			// display a very generic error to the user, and maybe send yourself an email
+			//ajax(["err"=>$e->getMessage()]);
 			return $this->error("Stripe ApiErrorException: ".$e->getMessage());
 		} catch (Exception $e) {
+			// something else happened, completely unrelated to Stripe
 			return $this->error("Exception: ".$e->getMessage());
 		}
 
