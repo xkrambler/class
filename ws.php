a39c5e8ff8aebd4499d060a906d38085f948deb8
xkrambler <github@xkr.es>
2022-03-21 14:42:21 +0100

    ws.php: WebService 0.1 common implementation

---
+++ ws.php
@@ -0,0 +1,237 @@
+<?php
+
+class WS {
+
+	public $data;
+	protected $o;
+
+	function __construct($o=[]) {
+		$this->o=$o;
+		$this->setup();
+	}
+	function __get($n) { return $this->o[$n]; }
+	function __set($n, $v) { $this->o[$n]=$v; }
+	function __isset($n) { return isset($this->o[$n]); }
+
+	// setup GET/POST/OPTIONS, allow all mods (including JSONP or AJAX)
+	function setup() {
+
+		// setup headers
+		header('Access-Control-Allow-Origin: *');
+		header("Allow: GET, POST, OPTIONS");
+		if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") exit;
+
+		// autoselect request type data
+		if (is_string($_REQUEST["action"])) {
+			$this->data=$_REQUEST;
+		} else if ($this->action=$GLOBALS["ajax"]) {
+			$this->data=$GLOBALS["adata"];
+		} else if (isset($_REQUEST["data"])) {
+			$this->data=json_decode($_REQUEST["data"], true);
+		} else {
+			$this->data=json_decode(file_get_contents('php://input'), true);
+		}
+		if (!isset($this->action)) $this->action=$this->data["action"];
+
+		// common non authenticated actions
+		switch ($this->action) {
+		case "timestamp":
+			$this->out(["timestamp"=>time()]);
+		case "microtime":
+			$this->out(["microtime"=>microtime(true)]);
+		case "ping":
+			$this->out(["pong"=>substr($this->data["ping"], 0, 256)]);
+		case "status":
+			if (isset($this->key)) $iskeyok=($key == $this->key);
+			else if (is_array($this->keys)) $iskeyok=$this->keys[$key];
+			$actions=($iskeyok && $this->keys[$key]?$this->keys[$key]:($iskeyok?["*"]:[]));
+			$this->out([
+				"iskeyok"=>$iskeyok,
+				"actions"=>$actions,
+				"ok"=>true,
+			]);
+		case "void":
+			$this->out();
+		case null:
+		case false:
+		case "":
+			$this->out(["err"=>"action not defined."]);
+		}
+
+	}
+
+	// private: output CSV value
+	private function outCSVvalue($v) {
+		return str_replace(
+			[";",'"',"\n"],
+			[",",'""',"|"],
+			$v
+		);
+	}
+
+	// private: plain text output
+	private function outPlain($d, $b=null) {
+		if ($b !== null) $b=$b.".";
+		foreach ($d as $n=>$v) {
+			if (is_array($v)) $this->outPlain($v, $b.$n);
+			else echo $b.$n."=".$v."\n";
+		}
+	}
+
+	// ensure authentication beyond this point
+	function auth() {
+
+		// get key from data
+		$key=$this->data["key"];
+
+		// if no key in data, try get it from headers
+		if (!isset($key)) {
+			$headers=getallheaders();
+			foreach ($keyheaders=["Authorization", "X-API-KEY"] as $k)
+				if (isset($headers[$k]))
+					$key=$headers[$k];
+		}
+
+		// ask for key
+		if (!is_string($key) && isset($_REQUEST["askkey"])) {
+			header("Content-Type: text/html; charset=UTF-8");
+			?><!doctype html>
+			<html lang='en'>
+			<head>
+				<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+			</head>
+			<body>
+				<form method='POST'>
+					Key: <input name='key' type='text' size='40' /> <input type='submit' value='Enter' />
+				</form>
+			</body>
+			</html><?php
+			exit;
+		}
+
+		// key/keys authentication
+		if (is_string($key)) {
+			if (isset($this->key) && $key === $this->key) return;
+			else if (is_array($this->keys) && is_array($this->keys[$key])) {
+				if (in_array($this->action, $this->keys[$key])) return;
+				else $this->out(["auth"=>"failed", "err"=>"Authorization key valid but failed for this action."]);
+			}
+			$this->out(["auth"=>"failed", "err"=>"Authorization key invalid."]);
+		}
+
+		// not beyond this point
+		$this->out(["auth"=>"required", "err"=>"No authorization key was provided for this action."]);
+		exit; // fallback
+
+	}
+
+	// check parameters
+	function check(array $param) {
+		foreach ($param as $p=>$t) {
+			if (!isset($this->data[$p])) $this->out(["err"=>"Required parameter (".$t."): ".implode(", ", array_keys($param)), "p"=>$p, "t"=>$t]);
+			else {
+				$nok=false;
+				switch ("".$t) {
+				case "json":
+					$this->data[$p]=@json_decode($this->data[$p], true);
+					break;
+				case "number":
+					if (is_numeric($this->data[$p])) $this->data[$p]=doubleval($this->data[$p]);
+					else $nok=true;
+					break;
+				case "email":
+					if (!filter_var($this->data[$p], FILTER_VALIDATE_EMAIL)) $nok=true;
+					break;
+				case "array":
+					if (!is_array($this->data[$p])) $nok=true;
+					break;
+				case "date":
+					if (!preg_match('/^(\d{4})-(\d{2})-(\d{2})$/', $this->data[$p], $v) || !checkdate($v[2], $v[3], $v[1])) $nok=true;
+					break;
+				default: // se acepta todo
+				}
+				if ($nok) $this->out(["err"=>"Parameter ".$p." does not meet required type specification: ".$t,"p"=>$p]);
+			}
+		}
+	}
+
+	// output
+	function out($d=null) {
+		if ($d === null) exit; // empty
+		if (is_string($_GET["jsonp"])) {
+			// clean/protect data
+			$data=$this->data;
+			unset($data["jsonp"]);
+			unset($data["key"]);
+			// send JSONP
+			header("Content-type: text/javascript");
+			echo $_GET["jsonp"]."(".json_encode($data).",".json_encode($d).");\n";
+			exit;
+		} else {
+			switch ($_REQUEST["out"]) {
+			case "html":
+				debug($d);
+				exit;
+			case "debug":
+				header("Content-type: text/plain");
+				var_dump($d);
+				exit;
+			case "plain":
+				header("Content-type: text/plain");
+				$this->outPlain($d);
+				exit;
+			case "csv":
+				header("Content-type: text/plain");
+				foreach ($d as $r) break;
+				if (!is_array($r)) $d=array($d);
+				$head=true;
+				foreach ($d as $r) {
+					if ($head) {
+						$index=0;
+						foreach ($r as $n=>$v)
+							echo ($index++?";":"").$this->outCSVvalue($n);
+						echo "\n";
+						unset($head);
+					}
+					$index=0;
+					foreach ($r as $n=>$v)
+						echo ($index++?";":"").$this->outCSVvalue($v);
+					echo "\n";
+				}
+				exit;
+			case "xml":
+				function array_to_xml($array, &$xml) {
+					foreach ($array as $k=>$v) {
+						if (is_array($v)) {
+							if (is_int($k)) $k="item";
+							$label=$xml->addChild($k);
+							array_to_xml($v, $label);
+						} else {
+							if (is_array($array) && is_int($k)) $k="item";
+							$xml->addChild($k, htmlspecialchars($v));
+						}
+					}
+				}
+				$xml=new SimpleXMLElement('<ws/>');
+				array_to_xml($d, $xml);
+				header("Content-type: text/xml; charset=UTF-8");
+				echo $xml->asXML();
+				exit;
+			case "json":
+			default:
+				ajax($d);
+			}
+		}
+	}
+
+	// dump database error
+	function dberr(dbbase $db) {
+		$this->out(["err"=>$db->driver()." #".$db->errnum().": ".$db->error()]);
+	}
+
+	// dump error
+	function err($error) {
+		$this->out(["err"=>$error]);
+	}
+
+}
