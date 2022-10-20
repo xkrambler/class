082ea4cb27ec1110f6a39e17b626f6a3248afd57
xkrambler <github@xkr.es>
2022-10-20 12:41:48 +0100

    xdata: ->get and ->set again, ->defaults

--- xdata.php
+++ xdata.php
@@ -8,6 +8,7 @@ class xData {
 	function __get(string $k) { return $this->o[$k]; }
 	function __set(string $k, $v) { $this->o[$k]=$v; }
 	function __isset(string $k) { return isset($this->o[$k]); }
+	function defaults() { return $this->defaults; }
 	function get() { return $this->o; }
-	function set(array $o) { foreach ($o as $k=>$v) $this->o[$k]=$v; }
+	function set(array $o=null) { if (is_array($o)) foreach ($o as $k=>$v) $this->o[$k]=$v; return $this->o; }
 }
