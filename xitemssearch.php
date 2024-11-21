269e3bf4f45e8001e4dc1212c8a6a7213c77e7d7
xkrambler <github@xkr.es>
2024-11-21 12:07:04 +0100

    xitemssearch: visible default for no parameters, bugfixes

--- xitemssearch.php
+++ xitemssearch.php
@@ -5,24 +5,24 @@ define("XITEMSSEARCH_DESC", 1);
 
 class xItemsSearch {
 
-	// helper para el ajax de consulta
+	// helper for query
 	static function query($o) {
 		$r=self::data($o);
 		if (is_array($r)) ajax(array_merge(array("ok"=>true), $r));
 		return $r;
 	}
 
-	// realizar consulta consulta y devolver como datos
+	// make query and return as data
 	static function data($o) {
 		$adata=($o["adata"]?$o["adata"]:$GLOBALS["adata"]);
 		$db=$o["db"];
 		$db_protocol=$db->protocol();
 
-		// inicialización
+		// parameters
 		$page=intval(isset($o["page"])?$o["page"]:$adata["page"]); if (!$page || $page < 0) $page=0;
-		$visible=intval(isset($o["visible"])?$o["visible"]:$adata["visible"]); if ($visible < 1 || $visible > 1000) $visible=0;
+		$visible=intval(isset($o["visible"])?$o["visible"]:(isset($adata["visible"])?$adata["visible"]:100)); if ($visible < 0) $visible=100;
 
-		// ordenación
+		// sorting
 		$sqlsort="";
 		if ($sort=($o["sort"]?$o["sort"]:$adata["sort"])) foreach ($sort as $f=>$t) {
 			if ($ordersf=$o["orders"][$f]) {
@@ -35,11 +35,11 @@ class xItemsSearch {
 			}
 		}
 
-		// verificar que la consulta lleve SQL_CALC_FOUND_ROWS si no tiene consulta de conteo
+		// ensure query has SQL_CALC_FOUND_ROWS if no count query
 		if (!isset($o["count"]) && !strpos($o["sql"], "SQL_CALC_FOUND_ROWS"))
-			ajax(array("err"=>"La consulta precisa SQL_CALC_FOUND_ROWS para ser contada!"));
+			ajax(array("err"=>"Query requires SQL_CALC_FOUND_ROWS!"));
 
-		// búsqueda
+		// search
 		if ($o["search"]) {
 			$search_options=($o["search_options"]?$o["search_options"]:array());
 			if ($o["translate"]) $search_options["translate"]=$o["translate"];
@@ -50,48 +50,46 @@ class xItemsSearch {
 			$search=$sqlsearch->sql();
 		}
 
-		// filtros por campos y límites
+		// filters by fields
 		$sqlfilters="";
-		$sqllimit="";
 		if ($adata["filters"])
 			foreach ($adata["filters"] as $field=>$value)
 				$sqlfilters.=" AND ".$db->sqlfield($field)."='".$db->escape($value)."'";
 
-		// consulta
-		$sqlquery=str_replace("%sqlsearch%", ($adata["search"]?$search:"1=1").$sqlfilters.$sqllimit, $o["sql"]);
+		// make query
+		$sqlquery=str_replace("%sqlsearch%", ($adata["search"]?$search:"1=1").$sqlfilters, $o["sql"]);
 
-		// limitar (sqlsrv)
-		if (!$o["nolimit"] && $db_protocol=="sqlsrv") {
+		// limit (sqlsrv)
+		if (!$o["nolimit"] && $db_protocol == "sqlsrv") {
 			switch ($o["querytype"]) {
 			default: if ($visible) $sqlquery="SELECT TOP ".$visible." * FROM (".$sqlquery.") xis_table";
 			}
 		}
 
-		// añadir ordenación ahora
+		// add order by
 		$sqlquery.=($sqlsort?" ORDER BY ".$sqlsort:"");
 
-		// limitar (mysql)
+		// limit (other)
 		if (!$o["nolimit"]) switch ($db_protocol) {
 			case "mysql": $sqlquery.=($visible?" LIMIT ".($page*$visible).",".$visible:""); break;
 			case "oracle": $sqlquery="SELECT * FROM (SELECT ROWNUM AS xis_rownum, xis_table.* FROM (".$sqlquery.") xis_table) xis_table2 WHERE xis_table2.xis_rownum BETWEEN ".($page*$visible)." AND ".($page*$visible+$visible); break;
 		}
 
-		// devolver consulta SQL generada, si solicitada
+		// return generated SQL, if asked
 		if ($o["getsql"]) return $sqlquery;
 
-		// realizar consulta
+		// start query
 		if ($o["timeout"]) $db->atimeout=$o["timeout"];
 		if (!$consulta=$db->query($sqlquery)) $db->err();
 		$timedout=($db->atimedout === true);
-		$datos=($timedout?[]:$consulta->aquery());
+		$rows=($timedout?array():$consulta->aquery());
 
-		// obtener conteo de filas
+		// get row count
 		$max=false;
 		if ($timedout) {
 			$max=0;
 		} else {
 			if (isset($o["count"])) {
-				// el conteo se realizará por consulta SQL autogenerada o específica
 				$sqlcount=($o["count"] === true
 					?"SELECT COUNT(*) FROM (".str_replace("%sqlsearch%", ($adata["search"]?$search:"1=1").$sqlfilters, $o["sql"]).") xis_tcount"
 					:str_replace("%sqlsearch%", ($adata["search"]?$search:"1=1").$sqlfilters, $o["count"])
@@ -101,23 +99,22 @@ class xItemsSearch {
 					$max=intval($db->field());
 				}
 			} else if (!$o["count"]) {
-				// por defecto, se contea con SQL_CALC_FOUND_ROWS
 				if (!$db->query("SELECT FOUND_ROWS()")) $db->err();
 				$max=intval($db->field());
 			}
 		}
 
-		// aplicar filtro, si especificado
-		if ($o["filter"]) $o["filter"]($datos);
+		// if any filter specified, apply
+		if ($o["filter"]) $o["filter"]($rows);
 
-		// devolver datos
+		// return data
 		return array_merge(($o["data"]?$o["data"]:array()), array(
 			"timeout"=>$o["timeout"],
 			"timedout"=>$timedout,
 			"page"=>$page,
 			"max"=>$max,
 			"visible"=>$visible,
-			"data"=>$datos,
+			"data"=>$rows,
 		));
 
 	}
